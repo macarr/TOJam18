d7ea347d50adf64b3102f17703d3d11acdf79b52
Max <contact.max.manco@gmail.com>
2018-05-04 17:09:28 -0400

    i did it' '

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+
+    public float cameraLimitTop;
+    public float cameraLimitBottom;
+    public float distanceFromPlayer;
+
+
+    public GameObject leadSkeleton;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (leadSkeleton != null)
+        {
+            transform.LookAt(leadSkeleton.transform);
+
+            transform.position = new Vector3(transform.position.x, transform.position.y, leadSkeleton.transform.position.z - distanceFromPlayer);
+
+            if (transform.position.z < cameraLimitBottom)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, cameraLimitBottom);
+            }
+
+            if (transform.position.z > cameraLimitTop)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y, cameraLimitTop);
+            }
+        }
+    }
+}
---
+++ Assets/PlayerMovementForward.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovementForward : MonoBehaviour {
+
+    public int positionInLine;
+
+    public float moveSpeed;
+    public float rotateSpeed;
+
+
+
+    public GameObject inFrontSkeleton;
+    public GameObject behindSkeleton;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (positionInLine == 0)
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Rotate(0,rotateSpeed,0);
+                transform.Translate(Vector3.forward * moveSpeed);
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Rotate(0, -rotateSpeed, 0);
+                transform.Translate(Vector3.forward * moveSpeed);
+            }
+        }
+
+        if (positionInLine != 0)
+        {
+            if (inFrontSkeleton != null)
+            {
+                if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+                {
+                    transform.LookAt(inFrontSkeleton.transform);
+                    transform.Translate(Vector3.forward * (moveSpeed * 1.0f));
+                }
+            }
+        }
+
+    }
+}
