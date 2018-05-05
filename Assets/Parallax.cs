5c810d05dca12960cd8d1c9418cd79e072712e27
Max <contact.max.manco@gmail.com>
2018-05-05 13:42:57 -0400

    adding sprites

---
+++ Assets/Parallax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+    public float parallaxSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(parallaxSpeed*Time.deltaTime, 0.0f, 0.0f);
+
+        if (transform.position.x >= 40.6)
+        {
+            transform.Translate(-60.9f, 0.0f, 0.0f);
+        }
+	}
+}
